eb3a2ac41d0b3029293fd4a42759246f734a1451
mohammad-anani <mohammad2006anani@gmail.com>
2025-07-27 16:50:45 +0300

    deleted forms

--- src/features/human-resources/patient/Form.tsx
+++
@@ -1,6 +0,0 @@
-import PersonForm from "../person/Form";
-
-export default function Form({ fieldPrefix = "" }: { fieldPrefix?: string }) {
-  const prefix = fieldPrefix + "";
-  return <PersonForm fieldPrefix={prefix} />;
-}
